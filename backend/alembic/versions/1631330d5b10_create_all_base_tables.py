333a45ba17b0661cf022ea1795da9638e53a343c
amanbendkule2001 <amanbendkule2001@gmail.com>
2026-08-22 15:16:17 +0530

    fix(alembic): linearize migration chain into a single head revision

--- backend/alembic/versions/1631330d5b10_create_all_base_tables.py
+++ backend/alembic/versions/1631330d5b10_create_all_base_tables.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '1631330d5b10'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = 'a1b2c3d4e5f6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
