43ee7ecd3dce2641e8b39b37d5314a9541f5d415
amanbendkule2001 <amanbendkule2001@gmail.com>
2026-08-22 16:23:44 +0530

    fix(alembic): set down_revision=None for root migration (remove ghost a1b2c3d4e5f6 reference)

--- backend/alembic/versions/1631330d5b10_create_all_base_tables.py
+++ backend/alembic/versions/1631330d5b10_create_all_base_tables.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '1631330d5b10'
-down_revision: Union[str, None] = 'a1b2c3d4e5f6'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
